1f2a14e8b3a6304a871ccddd28c13cf84ec13205
olesyash <ilana.olesya@gmail.com>
2015-12-11 11:36:23 +0200

    Added Setting Activity

--- app/src/main/java/com/example/olesya/quickpress/MainActivity.java
+++ app/src/main/java/com/example/olesya/quickpress/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.olesya.quickpress;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button settingsButton, startButton;
+    Context context;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settingsButton = (Button)findViewById(R.id.settingsButton);
+        startButton = (Button)findViewById(R.id.startButton);
+        context = this;
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SettingsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 
     @Override
@@ -29,9 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(context, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/olesya/quickpress/SettingsActivity.java
+++ app/src/main/java/com/example/olesya/quickpress/SettingsActivity.java
@@ -1,16 +1,43 @@
 package com.example.olesya.quickpress;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class SettingsActivity extends AppCompatActivity {
+    static final int MAX_LEVEL = 10;
+
+    TextView levelTextView, complexityTextView;
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        levelTextView = (TextView)findViewById(R.id.levelValueTextView);
+        complexityTextView = (TextView)findViewById(R.id.complexityValueTextView);
+
+        context = this;
+
+
+        levelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "here", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
