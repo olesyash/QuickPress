868b09f071472bb14fb01506fa22c57041a9499d
olesyash <ilana.olesya@gmail.com>
2015-12-18 17:54:22 +0200

    Added running timer

--- app/src/main/java/com/example/olesya/quickpress/MainActivity.java
+++ app/src/main/java/com/example/olesya/quickpress/MainActivity.java
@@ -2,49 +2,68 @@ package com.example.olesya.quickpress;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity implements GameInterface{
-    Button settingsButton, startButton;
-    Context context;
-    MyView myView;
+    private Button settingsButton, startButton, recentButton;
+    private Context context;
+    private MyView myView;
+    private TextView timerTextView, bestResultTextView;
+    private long startTime = 0;
+    private Handler timerHandler = new Handler();
+    private long bestResult = 0;
+    private boolean first = true;
+    private Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int milliseconds = (int)(millis - seconds*1000);
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%03d", seconds, milliseconds));
+            timerHandler.postDelayed(this, 100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         settingsButton = (Button)findViewById(R.id.settingsButton);
         startButton = (Button)findViewById(R.id.startButton);
-        context = this;
         myView = (MyView)findViewById(R.id.myView);
+        timerTextView = (TextView) findViewById(R.id.recentResultTextView);
+        recentButton = (Button) findViewById(R.id.recentResultButton);
+        bestResultTextView = (TextView)findViewById(R.id.bestResultTextView);
+
 
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, SettingsActivity.class);
                 startActivity(intent);
-
             }
         });
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //myView.getRandomCircle();
+                recentButton.setText("Current time:");
+                startTime = System.currentTimeMillis();
+                timerHandler.postDelayed(timerRunnable, 0);
                 myView.invalidate();
-                // /myView.postInvalidate();
-                //myView = new MyView(getApplicationContext());
-//                Toast.makeText(context, "x=" + myView.x + " y=" + myView.y, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -83,6 +102,17 @@ public class MainActivity extends AppCompatActivity implements GameInterface{
 
     @Override
     public void pressed() {
-        Toast.makeText(this,"Pressed", Toast.LENGTH_LONG).show();
+        timerHandler.removeCallbacks(timerRunnable);
+        recentButton.setText("Recent result");
+        Toast.makeText(this,"Stopped", Toast.LENGTH_LONG).show();
+        long millis = System.currentTimeMillis() - startTime;
+        if(first || millis < bestResult)
+        {
+            bestResultTextView.setText(timerTextView.getText());
+            bestResult = millis;
+            first = false;
+        }
+
+
     }
 }
